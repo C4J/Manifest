a6cb82747f4fe7596fecba8f8ad44136b2606af3
David Garratt <support@commander4j.com>
2023-07-22 17:24:55 +0100

    Updates

--- b6Manifest/src/com/commander4j/manifest/Create.java
+++ b6Manifest/src/com/commander4j/manifest/Create.java
@@ -12,10 +12,10 @@ import java.util.regex.Matcher;
 
 import org.apache.commons.io.FileUtils;
 
-public class Create
-{
+public class Create {
 
 	public static String[] excludes;
+	public static String[] manual_includes;
 	public static List<String> includes = new ArrayList<String>();
 	public static String source = "";
 	public static String startClass = "";
@@ -24,92 +24,90 @@ public class Create
 	public static BufferedWriter bw;
 	public static String prefix = "";
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 
-		if (args.length == 0)
-		{
+		if (args.length == 0) {
 			System.out.println("Syntax: b6Manifest manifest.name\n");
 			System.out.println("\n");
 			System.out.println("Parameter file contains a list parameters :-");
 			System.out.println("   Project base folder");
 			System.out.println("   Start Class");
 			System.out.println("   jars to exlude");
+			System.out.println("   manual includes to classpath");
 			System.out.println("\n");
 			System.exit(1);
-		}
-		else
-		{
+		} else {
 			File test = new File(args[0]);
-			if (test.exists())
-			{
-				try
-				{
+			if (test.exists()) {
+				try {
 					List<String> contents = FileUtils.readLines(test, "utf-8");
 					int lineNumber = 1;
 
 					// Iterate the result to print each line of the file.
-					for (String line : contents)
-					{
+					for (String line : contents) {
 
-						switch (lineNumber)
-						{
+						switch (lineNumber) {
 						case 1:
 							line = line.replaceFirst("^~", Matcher.quoteReplacement(System.getProperty("user.home")));
 							source = line;
 							break;
 						case 2:
 							startClass = line;
-							prefix = "Manifest-Version: 1.0\n" + "Main-Class: "+startClass+"\n" + "Class-Path:";
+							prefix = "Manifest-Version: 1.0\n" + "Main-Class: " + startClass + "\n" + "Class-Path:";
 							break;
 						case 3:
-							excludes = line.split(" ");
+							excludes = replaceNullStringwithBlank(line).split(" ");
+							break;
+						case 4:
+							manual_includes = replaceNullStringwithBlank(line).split(" ");
 							break;
 						default:
 							break;
 						}
 
-						if (lineNumber == 3)
-						{
-							System.out.println("Invoking b6Manifest on " + test);
-							System.out.println("   Folder = " + source);
-							topLevel(new File(source));
-							
-							Collections.sort(includes);
-						
-							fout = new File("MANIFEST.MF");
-							fos = new FileOutputStream(fout);
-							bw = new BufferedWriter(new OutputStreamWriter(fos));
-							
-							bw.write(prefix);
-							
-							for (int x=0;x<includes.size();x++)
-							{
-								bw.write(includes.get(x));
-								bw.newLine();
-								bw.flush();
-							}
-
-							bw.flush();
-							
-							fos.close();
-							fout=null;
-							
-							System.out.println("\nComplete.");
-
-							break;
-						}
 						lineNumber++;
 					}
-				}
-				catch (IOException e)
-				{
+
+					if (lineNumber >= 3) {
+						System.out.println("Invoking b6Manifest on " + test);
+						System.out.println("   Folder = " + source);
+						System.out.println("");
+						topLevel(new File(source));
+
+						if (manual_includes != null) {
+							for (int x = 0; x < manual_includes.length; x++) {
+								includes.add(" " + manual_includes[x] + " ");
+							}
+						}
+
+						Collections.sort(includes);
+
+						fout = new File("MANIFEST.MF");
+						fos = new FileOutputStream(fout);
+						bw = new BufferedWriter(new OutputStreamWriter(fos));
+						System.out.println(prefix);
+						bw.write(prefix);
+
+						for (int x = 0; x < includes.size(); x++) {
+							bw.write(includes.get(x));
+							System.out.println(includes.get(x));
+							bw.newLine();
+							bw.flush();
+						}
+
+						bw.flush();
+
+						fos.close();
+						fout = null;
+
+						System.out.println("\nComplete.");
+
+					}
+				} catch (IOException e) {
 					e.printStackTrace();
 				}
 
-			}
-			else
-			{
+			} else {
 				System.out.println(test.getAbsolutePath() + " not found.\n");
 				System.out.println("\n");
 				System.exit(1);
@@ -118,63 +116,53 @@ public class Create
 		System.exit(0);
 	}
 
-	public static void topLevel(File sourceFile)
-	{
+	public static void topLevel(File sourceFile) {
 
 		dirTree(sourceFile);
 
 	}
 
-	public static void dirTree(File dir)
-	{
+	public static void dirTree(File dir) {
 
 		File[] subdirs = dir.listFiles();
-		for (File subdir : subdirs)
-		{
-			if (subdir.isDirectory())
-			{
+		for (File subdir : subdirs) {
+			if (subdir.isDirectory()) {
 				dirTree(subdir);
-			}
-			else
-			{
+			} else {
 				doFile(subdir);
 			}
 		}
 	}
 
-	public static void doFile(File file)
-	{
-		if (file.getAbsolutePath().endsWith(".DS_Store") == false)
-		{
+	public static String replaceNullStringwithBlank(String value) {
+		if (value == null) {
+			value = "";
+		}
 
-			if (file.getAbsolutePath().endsWith(".jar") == true)
-			{
+		return value;
+	}
+
+	public static void doFile(File file) {
+		if (file.getAbsolutePath().endsWith(".DS_Store") == false) {
+
+			if (file.getAbsolutePath().endsWith(".jar") == true) {
 
 				boolean include = true;
-				if (excludes.length == 0)
-				{
+				if (excludes.length == 0) {
 					include = true;
-				}
-				else
-				{
-					for (int x = 0; x < excludes.length; x++)
-					{
-						if (file.getAbsolutePath().toLowerCase().endsWith(excludes[x].toLowerCase()))
-						{
+				} else {
+					for (int x = 0; x < excludes.length; x++) {
+						if (file.getAbsolutePath().toLowerCase().endsWith(excludes[x].toLowerCase())) {
 							include = false;
 						}
 					}
 				}
 
-				if (include)
-				{
+				if (include) {
 
-					try
-					{
-						includes.add(" "+file.getAbsolutePath().substring(source.length() + 1) + " ");
-					}
-					catch (Exception e)
-					{
+					try {
+						includes.add(" " + file.getAbsolutePath().substring(source.length() + 1) + " ");
+					} catch (Exception e) {
 
 						e.printStackTrace();
 					}
